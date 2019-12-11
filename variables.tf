d6e41d140e58e7792bd8d432dc1a33e89d8f2fce
local1 <cristian.beraha@e.lastic.guru>
2019-12-11 20:55:14 -0300

    version close

--- variables.tf
+++ variables.tf
@@ -12,20 +12,3 @@ variable "environment_type" {
   default     = ""
 }
 
-
-variable "organization_email" {
-  description = "Organization e-mail for root id"
-}
-
-variable "organization_allow_billing_access" {
-  description = "Allow Billing access to organization structure sub-accounts"
-  default     = false
-}
-
-variable "organization_role" {
-  default = "OrganizationAllowAllAccess"
-}
-
-variable "aws_region" {
-  default = "us-east-1"
-}
